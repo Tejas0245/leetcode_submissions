ddc91381c997520889a157a439232790186e2508
Tejas0245 <102210042+Tejas0245@users.noreply.github.com>
2024-06-10 12:33:31 +0530

    Time: 2 ms (88.93%), Space: 41.1 MB (65.80%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        // int[] expected = new int[heights.length];
+      int[] expected = Arrays.copyOf(heights,heights.length);
+
+        Arrays.sort(expected);
+        int count = 0;
+        for(int i = 0;i<heights.length;i++){
+          
+            
+            if(heights[i]!=expected[i]){
+                count++;
+            }
+        }
+        return count;
+        
+    }
+}
